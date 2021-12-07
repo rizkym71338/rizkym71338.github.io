fa7a873254db01cdf77434fd92cf9b37d47f035b
rizkym71338 <rizkym71338@gmail.com>
2021-12-07 19:47:43 +0700

    initial commit

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -10,12 +10,12 @@ const Home = () => {
                     <div className="h-full">
                         <IndexHero />
                         <div className="h-screen flex justify-center items-center">
-                            <div className="flex z-50 lg:text-7xl text-xl font-bold text-gray-200 bg-gray-800 bg-clip-padding lg:w-1/2 lg:h-1/2 lg:p-4 w-2/3 py-20  rounded-3xl backdrop-filter backdrop-blur-sm bg-opacity-0 justify-center items-center border-white border border-opacity-5">
+                            <div className="flex z-50 md:text-4xl text-xl font-bold text-gray-200 bg-gray-800 bg-clip-padding md:w-1/2 md:h-1/2 md:p-4 w-2/3 py-20  rounded-3xl backdrop-filter backdrop-blur-sm bg-opacity-0 justify-center items-center border-white border border-opacity-5">
                                 <AutoType text="Wellcome !!!" />
                             </div>
                         </div>
                         <div className="h-screen flex justify-center items-center">
-                            <div className="flex z-50 lg:text-5xl text-xl text-gray-200 font-bold">
+                            <div className="flex z-50 md:text-5xl text-xl text-gray-200 font-bold">
                                 Coming Soon :)
                             </div>
                         </div>
